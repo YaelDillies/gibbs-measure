3b08701a8a24f4768c8e4de79c0472dedc5accb7
Yaël Dillies <yael.dillies@gmail.com>
2026-04-05 17:55:37 +0200

    revert

--- GibbsMeasure/Mathlib/MeasureTheory/Function/ConditionalExpectation/Basic.lean
+++ GibbsMeasure/Mathlib/MeasureTheory/Function/ConditionalExpectation/Basic.lean
@@ -41,6 +41,20 @@ theorem toReal_ae_eq_indicator_condExp_of_forall_setLIntegral_eq (hm : m ≤ m0)
   · exact hgm.stronglyMeasurable_mk.measurable.ennreal_toReal.aestronglyMeasurable.congr
       (hgm.ae_eq_mk.fun_comp ENNReal.toReal).symm
 
+-- /-- **Uniqueness of the conditional expectation**
+-- If a function is a.e. `m`-measurable, verifies an integrability condition and has same integral
+-- as `f` on all `m`-measurable sets, then it is a.e. equal to `μ[f|hm]`. -/
+-- theorem toReal_ae_eq_condExp_toReal_of_forall_setLIntegral_eq (hm : m ≤ m0)
+--     [SigmaFinite (μ.trim hm)] {f g : α → ℝ≥0∞} (hf_meas : AEMeasurable f μ)
+--     (hf : ∫⁻ a, f a ∂μ ≠ ⊤)
+--     (hg_int_finite : ∀ s, MeasurableSet[m] s → μ s < ∞ → ∫⁻ a in s, g a ∂μ ≠ ⊤)
+--     (hg_eq : ∀ s : Set α, MeasurableSet[m] s → μ s < ∞ → ∫⁻ x in s, g x ∂μ = ∫⁻ x in s, f x ∂μ)
+--     (hgm : AEStronglyMeasurable[m] g μ) :
+--     (fun a ↦ (g a).toReal) =ᵐ[μ] μ[fun a ↦ (f a).toReal|m] := by
+--   refine ae_eq_condExp_of_forall_setIntegral_eq hm
+--     (integrable_toReal_of_lintegral_ne_top hf_meas hf)
+--     (fun s hs hs_fin ↦ integrable_toReal_of_lintegral_ne_top _ _) _ _
+
 lemma toReal_ae_eq_indicator_condExp_iff_forall_meas_inter_eq (hm : m ≤ m0)
     [SigmaFinite (μ.trim hm)] {g : α → ℝ≥0∞} {s : Set α} :
     (fun a ↦ (g a).toReal) =ᵐ[μ] μ[s.indicator 1| m] ↔
